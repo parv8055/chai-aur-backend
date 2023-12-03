fda790c5b2b6615e4990458acf57a350797b1081
parv8055 <parvchoudhary4@gmail.com>
2023-12-03 15:35:09 +0530

    connected to mongodb

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+import {connect} from "mongoose";
 import { db_name } from "../constants.js";
 
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`)
+        const connectionInstance = await connect(`${process.env.MONGO_URL}/${db_name}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
+        console.error("MONGODB connection FAILED ", error);
         process.exit(1)
     }
 }
