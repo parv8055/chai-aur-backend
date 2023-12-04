918033c94fe6a4b190c7686e261c1c5fa2a69903
parv8055 <parvchoudhary4@gmail.com>
2023-12-04 12:12:00 +0530

    added some middlewares

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,10 @@
 import {connect} from "mongoose";
 import { db_name } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
         const connectionInstance = await connect(`${process.env.MONGO_URL}/${db_name}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MONGODB connection FAILED ", error);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
-connectDB()
+  path: "./env",
+});
+app.on("error", (err) => {
+  console.error(`Error in app: ${err}`);
+  throw err;
+});
+const port = process.env.PORT || 3000;
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`😇 App is running at port ${port}`);
+  });
+});
