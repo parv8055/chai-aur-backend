80155e7b162cd51aec8d7cd9e614dfb8a7b29caf
parv8055 <choudharyparv4@gmail.com>
2023-12-05 14:38:56 +0530

    jwt and custom methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,13 +61,14 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+//custom methods
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
       email: this.email,
       userName: this.userName,
-      userName: this.userName,
+      fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
@@ -75,6 +76,8 @@ userSchema.methods.generateAccessToken = function () {
     }
   );
 };
+
+//custom methods
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
