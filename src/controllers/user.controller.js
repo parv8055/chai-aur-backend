30a86384eb064d6de7a0d666a9385ad437f8d43b
parv8055 <choudharyparv4.com>
2023-12-16 14:22:51 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,20 +18,27 @@ export const registerUser = asynchandler(async (req, res, next) => {
   const existedUser = await User.findOne({
     $or: [{ userName }, { email }],
   });
-  
+
   if (existedUser) {
-    throw new ApiError(409, "User with eamil or username exists");
+    throw new ApiError(409, "User with email or username exists");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
-  
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-  
+
   if (!avatar) {
     throw new ApiError(400, "Avatar file is required");
   }
-  
+
   // return
   const user = await User.create({
     userName: userName.toLowerCase(),
@@ -48,5 +55,7 @@ export const registerUser = asynchandler(async (req, res, next) => {
   if (!createdUser) {
     throw new ApiError(500, "something went wrong while registering the user");
   }
-  return res.status(201).json(new ApiResponse(201, createdUser, "User registered"));
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered"));
 });
